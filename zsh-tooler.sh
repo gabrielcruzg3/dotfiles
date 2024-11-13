ca1c2b266a3a7a6491aa93a35103319e4e7698de
Gabriel Cruz <cruzg3dev@gmail.com>
2024-11-13 03:03:55 -0300

    fix: Update zsh-tooler.sh to use sudo for theme and plugin management

--- zsh-tooler.sh
+++ zsh-tooler.sh
@@ -1,17 +1,17 @@
 sudo apt install zsh git -y
 
-chsh -s $(which zsh)
 sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)" && exit
 
-rm -rf $ZSH_CUSTOM/themes/spaceship-prompt/
-git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
+sudo rm -rf $ZSH_CUSTOM/themes/spaceship-prompt/
+sudo rm -f $ZSH_CUSTOM/themes/spaceship.zsh-theme
 
-rm -f /home/g3/.oh-my-zsh/custom/themes/spaceship.zsh-theme     
+git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
 ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
 
-rm -rf /home/g3/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting    
-git clone -f https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+sudo rm -rf  $ZSH_CUSTOM/plugins/zsh-syntax-highlighting    
+sudo rm -rf  $ZSH_CUSTOM/plugins/zsh-autosuggestions
 
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
 
-rm -rf /home/g3/.oh-my-zsh/custom/plugins/zsh-autosuggestions
-git clone -f https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+chsh -s $(which zsh)
