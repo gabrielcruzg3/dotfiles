e2a7a51c36c084d3d9b6959ee454fb51c1d86c1d
Gabriel Cruz <cruzg3dev@gmail.com>
2025-01-22 23:46:57 -0300

    refactor: Enhance asdf.sh with modular functions for language installation and improved usage options

--- asdf.sh
+++ asdf.sh
@@ -1,35 +1,82 @@
 #!/bin/bash
 
-# Ensure asdf is installed
-if [ ! -d "$HOME/.asdf" ]; then
-  echo "asdf not found, cloning repository..."
-  git clone https://github.com/asdf-vm/asdf.git ~/.asdf
-else
-  echo "asdf already installed."
-fi
+dotnet() {
+  asdf plugin-add dotnet-core https://github.com/emersonsoares/asdf-dotnet-core.git
+  asdf install dotnet-core latest
+  asdf global dotnet-core latest
+}
 
-# Source asdf
-echo "Sourcing asdf..."
-. $HOME/.asdf/asdf.sh
+node() {
+  asdf plugin-add nodejs https://github.com/asdf-vm/asdf-nodejs.git
+  asdf install nodejs latest
+  asdf global nodejs latest
+}
 
-# Add asdf plugins
-echo "Adding asdf plugins..."
-asdf plugin-add nodejs https://github.com/asdf-vm/asdf-nodejs.git
-asdf plugin-add java https://github.com/halcyon/asdf-java.git
-asdf plugin-add dotnet-core https://github.com/emersonsoares/asdf-dotnet-core.git
+java() {
+  asdf plugin-add java https://github.com/halcyon/asdf-java.git
+  asdf install java latest
+  asdf global java latest
+}
 
-# Install specific versions
-echo "Installing latest versions of Node.js, Java, and .NET..."
-asdf install nodejs latest
-asdf install java latest
-asdf install dotnet-core latest
+all() {
+  echo "Installing latest versions of Node.js, Java, and .NET..."
+  dotnet
+  node
+  java
+  echo "Node.js, Java, and .NET have been set up using asdf."
+}
 
-# Set global versions
-echo "Setting global versions for Node.js, Java, and .NET..."
-asdf global nodejs latest
-asdf global java latest
-asdf global dotnet-core latest
+basic() {
+  node
+  dotnet
+}
 
-echo "Node.js, Java, and .NET have been set up using asdf."
+usage() {
+  echo "Usage: $0 [options]"
+  echo "Options:"
+  echo "  --all                 Run all steps"
+  echo "  --basic               Install only Node.js and .NET"
+  echo " --[dotnet|node|java]   Install specific language"
+  echo "  --help                Display this help message"
+}
+
+setup() {
+  # Ensure asdf is installed
+  if [ ! -d "$HOME/.asdf" ]; then
+    echo "asdf not found, cloning repository..."
+    git clone https://github.com/asdf-vm/asdf.git ~/.asdf
+  else
+    echo "asdf already installed."
+  fi
+
+  # Source asdf
+  echo "Sourcing asdf..."
+  . $HOME/.asdf/asdf.sh
+
+  # Add asdf plugins
+  echo "Adding asdf plugins..."
+}
+
+main() {
+  if [ "$#" -eq 0 ]; then
+    usage
+    exit 1
+  fi
+
+  setup
+
+  case "$1" in
+  --all) all ;;
+  --basic) basic ;;
+  --dotnet) dotnet ;; --node) node ;; --java) java ;; --help) usage ;;
+  *)
+    echo "Invalid option: $1"
+    usage
+    exit 1
+    ;;
+  esac
+}
+
+main "$@"
 
 exit
